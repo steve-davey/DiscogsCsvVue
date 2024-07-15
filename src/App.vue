30bedb62df11fc430daae0c6e937fe0cf94d7218
Steve Davey <double.happiness@outlook.com>
2024-07-15 17:24:52 +0100

    import prepareDownload

--- src/App.vue
+++ src/App.vue
@@ -9,23 +9,18 @@
         </p>
     </div>
 
-    <div>
-    <PrepareDownload />
-  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import FileUpload from '@/components/FileUpload.vue';
 import { fetchRelease, parseCsvToArray } from "@/parser";
-import PrepareDownload from './components/PrepareDownload';
-import { ROW_NAMES } from '@/components/RowNames';
+import prepareDownload from './components/PrepareDownload';
 
 export default defineComponent({
     name: 'App',
     components: {
         FileUpload,
-        PrepareDownload
     },
     data() {
         return {
@@ -46,13 +41,7 @@ export default defineComponent({
             }
         },
         async downloadCSV(releases: any[]) {
-            const csvContent = "releases:text/csv;charset=utf-8," + ROW_NAMES.join(",") + "\n" + releases.map(e => e.join(",")).join("\n");
-            const encodedUri = encodeURI(csvContent);
-            const link = document.createElement("a");
-            link.setAttribute("href", encodedUri);
-            link.setAttribute("download", "my_data.csv");
-            document.body.appendChild(link); // Required for Firefox
-            link.click();
+            download = await prepareDownload(releases)
         }
     },
     watch: {
